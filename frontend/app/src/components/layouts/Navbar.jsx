86e72eb4720178a5730f410c910cd32c1f5b5b51
xyz <xyz.@example.com>
2025-08-28 17:47:44 +0530

    changed stylings

--- frontend/app/src/components/layouts/Navbar.jsx
+++ frontend/app/src/components/layouts/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className="h-16 bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-2.5 px-4 md:px-0 sticky top-0  z-30">
             <div className="container mx-auto max-w-7xl px-4 md:px-0 flex justify-between items-center gap-5">
                 <Link to="/dashboard">
-                    <h2 className="text-lg  md:text-xl font-medium text-black leading-5">
+                    <h2 className="text-lg  md:text-xl font-medium text-black-500">
                         Interview Prep AI
                     </h2>
                 </Link>
